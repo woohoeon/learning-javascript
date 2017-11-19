43b079c139f3ec0ecf0ddcab2fb44222655f8885
ho-un Woo <woohoeon@gmail.com>
2017-11-19 18:05:28 +0900

    Updat chapter8

--- chapter8/test.js
+++ chapter8/test.js
@@ -213,4 +213,139 @@
     cards.filter(c => c.value === 2);
     console.log(cards.filter(c => c.value === 2));
 
+    /**
+     * reduce
+     */
+    // reduce는 콜백 함수를 받습니다. 콜백에서 첫 번째 매개변수는 항상 현재 배열 요소였지만, reduce는 다릅니다.
+    // reduce가 받는 첫번째 매개변수는 배열이 줄어드는 대상인 어큐뮬레이터(accumulator) 입니다. 
+    // 두 번째 매개변수부터는 콜백의 순서대로 현재 배열 요소, 현재 인덱스, 배열 자체입니다.
+    const arr16 = [5, 7, 2, 4];
+    const sum = arr16.reduce((a, x) => a += x, 0);
+    console.log(sum);
+    // a에 값을 할당할 필요는 없습니다.
+    // 함수에서 중요한 건 무엇을 반환하는가 이므로 그냥 a+x를 반환해도 됐을 겁니다.
+    // 하지만 reduce를 더 잘 활용하려면 누적값이 어떻게 변하는지 생각하는 습관을 기르는게 좋습니다.
+
+    // reduce는 보통 숫자나 문자열 같은 원시 값을 누적값으로 사용하지만, 객체 또한 누적값이 될 수 있습니다.
+    const words = ['Beachball', 'Rodeo', 'Angel', 'Aardvark', 'Xylophone', 'November', 'Chocolate', 'Papay', 'Uniform', 'Joker', 'Clover', 'Bali'];
+    const alphabetical = words.reduce((a, x) => {
+        if (!a[x[0]]) a[x[0]] = [];
+        a[x[0]].push(x);
+        return a;
+    }, {});
+    console.log(alphabetical); // {B: ["Beachball", "Bali"], R: ["Rodeo"], A: ["Angel", "Aardvark"], X: ["Xylophone"], N: ["November"], …}
+
+    // reduce는 통계에서도 사용할 수 있습니다. 예를 들어 데이터 셋의 평균(mean)과 분산(variance)을 계산 한다고 해 봅시다.
+    const data = [3, 3, 5, 7.2, 12, 4, 6, 10.3];
+    // 도널드 커누스가 분산 계산을 위해 만든 알고리즘입니다.
+    const stats = data.reduce((a, x) => {
+        a.N++;
+        let delta = x - a.mean;
+        a.mean += delta / a.N;
+        a.M2 += delta * (x - a.mean);
+        return a;
+    }, { N: 0, mean: 0, M2: 0 });
+    if (stats.N > 2) {
+        stats.variance = stats.M2 / (stats.N - 1);
+        stats.stdev = Math.sqrt(stats.variance);
+    }
+    console.log(stats); // {N: 8, mean: 6.3125, M2: 78.14875, variance: 11.164107142857144, stdev: 3.3412732816782804}
+
+    // reduce의 유연성을 알아보기 위해 한 가지 예제를 더 살펴보겠습니다. 예제 자체는 매우 조악하지만, 이번예는 문자열을 누적값으로 사용합니다.
+    // trim() 메소드는 양끝의 공백을 제거한 문자열을 반환합니다. trim()은 그 문자열 자체 값에는 영향을 끼치지 않습니다. 
+    const longWords = words.reduce((a, w) => w.length > 6 ? a + ' ' + w : a, '').trim();
+    console.log(longWords);
+
+    // reduce 대신 fileter와 join을 써써 같은 결과를 얻을 수 있습니다.
+    const longWord2 = words.filter(x => x.length > 6).join(' ');
+    console.log(longWord2);
+
+    /**
+     * 삭제되거나 정의되지 않은 요소들
+     */
+    // map과 filter, reduce는 삭제되거나 정의되지 않은 요소들에서 콜백 함수를 호출하지 않습니다.
+    const arr17 = [1, 2, 3, 4, 5];
+    delete arr17[2];
+    const nArr17 = arr17.reduce((a, x) => {
+        if (!a) a = [];
+        a.push(x);
+        return a;
+    }, []);
+    console.log(nArr17); // [1, 2, 4, 5]
+    // 의도적으로 배열안에 빈 부분을 만든다 하더라도 배열에 delete를 쓰지는 않을 테니 현실적으로는 이런 동작이 문제를 일으킬 가능성은 거의 없습니다.
+
+    /**
+     * 문자열 병합
+     */
+    {
+        // Array.prototype.join은 매개변수로 구분자 하나를 받고 요소들을 하나로 합친 문자열을 반환합니다.
+        // 매개변수의 기본값은 쉼표이며, 정의되지 않은 요소, 삭제된 요소, null, undefined는 모두 빈 문자열로 취급합니다.
+        const arr = [1, null, 'hello', 'world', true, undefined];
+        delete arr[3];
+        arr.join(); // 1,,hello,,true,
+        console.log(arr.join());
+        arr.join(''); // 1hellotrue
+        console.log(arr.join(''));
+        arr.join(' -- '); // 1 --  -- hello --  -- true -- 
+        console.log(arr.join(' -- '));
+
+        // 문자열 병합과 Array.prototype.join을 함께 쓰면 HTML<ul>리스트 같은 것도 만들 수 있습니다.
+        const attributes = ['Nimble', 'Perceptive', 'Generous'];
+        const html = '<ul><li>' + attributes.join('</li><li>') + '</li></ul>';
+        console.log(html); // <ul><li>Nimble</li><li>Perceptive</li><li>Generous</li></ul>
+    }
+
+    /**
+     * 요약
+     * 
+     * 배열 함수의 매개변수(순서대로)
+     * --------------------------------------------------------------
+     * 매서드              | 설명
+     * --------------------------------------------------------------
+     * reduce에만 적용 | 누적값, 초깃값 또는 마지막 호출에서 반환하는 값
+     * 모든 메서드        | 요소(현재 요소의 값)
+     * 모든 메서드        | 현재 요소의 인덱스
+     * 모든 메서드        | 배열 자체(그다지 쓸모는 없음)
+     * --------------------------------------------------------------
+     * 콜백을 받는 메서드들은 또한 옵션으로 콜백을 호출할 때 this로 사용할 값을 받을 수 있습니다.
+     * 이 매개변수를 활용하면 콜백 함수를 메서드처럼 사용 할 수 있습니다.
+     * 
+     * 배열 콘텐츠 조작
+     * --------------------------------------------------------------
+     * 하고 싶은 일 | 사용할 메서드 | 수정 또는 사본
+     * --------------------------------------------------------------
+     * 스택(후입 선출)을 만들 때 | push(바뀐 길이 반환), pop | 배열수정
+     * 큐(선입 선출)를 만들 때 | unshift(바뀐 길이 반환), shift | 배열수정
+     * 여러 요소를 배열 마지막에 추가할 때 | const | 사본반환
+     * 배열 일부가 필요할 때 | slice | 사본반환    
+     * 임의의 위치에 요소를 추가하거나 제거할 때 | splice | 배열수정
+     * 배열 안에서 요소를 교체할 때 | copyWithin | 배열수정
+     * 배열을 채울 때 | fill | 배열수정
+     * 배열을 반대로 정렬할 때 | reverse | 배열수정
+     * 배열을 정렬할 때 | sort(정렬 함수 사용 가능) | 배열수정
+     * --------------------------------------------------------------
+     * 
+     * 배열 검색
+     * --------------------------------------------------------------
+     * 찾고자 하는 것 | 사용할 메서드
+     * --------------------------------------------------------------
+     * 요소의 인덱스 | indexOf(단순한 값), findIndex(복잡한 값)
+     * 인덱스 뒤에서부터 찾을 때 | lastIndexOf(단순한 값)
+     * 요소 자체 | find
+     * 조건을 만족하는 요소가 들어있는지 확인할 때 | some
+     * 모든 요소가 그 조건을 만족하는지 확인할 때 | every
+     * --------------------------------------------------------------
+     * 
+     * 배열변형
+     * --------------------------------------------------------------
+     * 하고 싶은 일 | 사용할 메서드 | 수정 또는 사본
+     * --------------------------------------------------------------
+     * 배열의 모든 요소를 변형할 때 | map | 사본반환
+     * 조건에 맞는 요소만 남길 때 | filter | 사본반환
+     * 배열 전체를 다른 데이터 타입으로 변형할 때 | reduce | 사본반환
+     * 요소를 문자열로 바꿔서 하나로 합칠 때 | join | 사본반환
+     * --------------------------------------------------------------
+     */
+
+
 })();
